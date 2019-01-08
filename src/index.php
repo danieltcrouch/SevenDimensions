bd8af5de0849bc8c27d1119add3479498ab080da
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-01-08 09:56:30 -0600

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,35 @@
+<?php include_once($_SERVER["DOCUMENT_ROOT"] . "/utility/common/php/startup.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Seven Dimensions</title>
+    <?php include($BASE_UTL_PATH . "/common/html/head.html"); ?>
+</head>
+
+<body>
+
+	<!--Header-->
+    <?php include($BASE_UTL_PATH . "/common/html/header.html"); ?>
+    <div class="col-10 header">
+        <div class="title center"><span class="clickable">
+            Seven Dimensions
+            <img style="width: .5em; padding-bottom: .25em" src="http://religionandstory.webutu.com/utility/common/images/question-mark.png" alt="help">
+        </span></div>
+        <div id="instructions" style="display: none">
+            This is a supplemental tool for the board game <i>Seven Dimensions</i>.<br/><br/>
+            It can be used for keeping track of War Bucks awarded each turn as well as advancements.
+            It can also be used for sandboxing games.
+        </div>
+    </div>
+
+    <!--Main-->
+    <div class="col-10 main">
+        <div class="title center">This Page is under construction</div>
+        <br />
+        <div class="center"><img src="https://image.freepik.com/free-icon/traffic-cone-signal-tool-for-traffic_318-62079.jpg" width="300px"></div>
+    </div>
+
+</body>
+<?php include($BASE_UTL_PATH . "/common/html/modal.html"); ?>
+<?php include($BASE_UTL_PATH . "/common/html/toaster.html"); ?>
+</html>
