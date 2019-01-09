8c244ccd1192cac244d810cb7449d00bd5cef63e
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-01-09 11:03:49 -0600

    Integrated new architecture

--- src/index.php
+++ src/index.php
@@ -1,19 +1,19 @@
-<?php include_once($_SERVER["DOCUMENT_ROOT"] . "/utility/common/php/startup.php"); ?>
+<?php include("$_SERVER[DOCUMENT_ROOT]/common/php/startup.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Seven Dimensions</title>
-    <?php include($BASE_UTL_PATH . "/common/html/head.html"); ?>
+    <?php includeHeadInfo(); ?>
 </head>
 
 <body>
 
 	<!--Header-->
-    <?php include($BASE_UTL_PATH . "/common/html/header.html"); ?>
+    <?php includeHeader(); ?>
     <div class="col-10 header">
         <div class="title center"><span class="clickable">
             Seven Dimensions
-            <img style="width: .5em; padding-bottom: .25em" src="http://religionandstory.webutu.com/utility/common/images/question-mark.png" alt="help">
+            <img style="width: .5em; padding-bottom: .25em" src="<?php getHelpImage() ?>" alt="help">
         </span></div>
         <div id="instructions" style="display: none">
             This is a supplemental tool for the board game <i>Seven Dimensions</i>.<br/><br/>
@@ -26,10 +26,9 @@
     <div class="col-10 main">
         <div class="title center">This Page is under construction</div>
         <br />
-        <div class="center"><img src="https://image.freepik.com/free-icon/traffic-cone-signal-tool-for-traffic_318-62079.jpg" width="300px"></div>
+        <div class="center"><img src="<?php getConstructionImage(); ?>" width="300px"></div>
     </div>
 
 </body>
-<?php include($BASE_UTL_PATH . "/common/html/modal.html"); ?>
-<?php include($BASE_UTL_PATH . "/common/html/toaster.html"); ?>
+<?php includeModals(); ?>
 </html>
