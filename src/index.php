ed89b24e76683d5c035b7c3192b7d6841a11327f
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-07-17 11:44:04 -0500

    Updated Help functionality

--- src/index.php
+++ src/index.php
@@ -12,9 +12,9 @@
     <div class="col-10 header">
         <div class="title center"><span class="clickable">
             Seven Dimensions
-            <img style="width: .5em; padding-bottom: .25em" src="<?php getHelpImage() ?>" alt="help">
+            <img id="helpIcon" style="width: .5em; padding-bottom: .25em" src="<?php getHelpImage() ?>" alt="help">
         </span></div>
-        <div id="instructions" style="display: none">
+        <div id="helpText" style="display: none">
             This is a supplemental tool for the board game <i>Seven Dimensions</i>.<br/><br/>
             It can be used for keeping track of War Bucks awarded each turn as well as advancements.
             It can also be used for sandboxing games.
